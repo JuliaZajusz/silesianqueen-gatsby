a3c9973533341f74bdf917ac449147560695f971
Julia Zajusz <julia.zajusz@gmail.com>
2022-05-03 11:23:55 +0200

    first jumbotron

--- src/components/components/Jumbotron.tsx
+++ src/components/components/Jumbotron.tsx
@@ -1,13 +1,35 @@
 import React from "react";
+import Button from '@mui/material/Button'
+import { createStyles } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
+const useStyles = makeStyles((theme) => ({
+    jumbotron: {
+        minHeight: '100vh',
+        display: "grid",
+        placeItems: 'center',
+        paddingBottom: 60,
+        background: "url(https://source.unsplash.com/random) no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+    },
+}));
 
 interface JumbotronProps {
+    title: string
 }
 
-const Jumbotron: React.FC = (props: JumbotronProps) => {
+const Jumbotron: React.FC<JumbotronProps> = (props) => {
+    const { title } = props;
+    const classes = useStyles();
 
-return (
-    <div>
-    </div>
+    return (
+        <div className={classes.jumbotron}>
+            {title}
+            <Button variant="contained">Hello World</Button>
+        </div>
     )
 }
 
